01ef950bd071e4d9ce7610b453a3de4053f2f267
Thomas Dutrion <thomas@engineor.com>
2017-02-16 08:56:13 +0100

    Add consistency to method chaining DocBlocks

--- src/AbstractAdapter.php
+++ src/AbstractAdapter.php
@@ -18,6 +18,7 @@ use Zend\Validator\AbstractValidator;
  * Provides some utility functionality to build on
  */
 abstract class AbstractAdapter extends AbstractValidator implements AdapterInterface
+
 {
     /**
      * Captcha name
@@ -58,7 +59,7 @@ abstract class AbstractAdapter extends AbstractValidator implements AdapterInter
      * Set name
      *
      * @param string $name
-     * @return AbstractAdapter
+     * @return AbstractAdapter Provides a fluent interface
      */
     public function setName($name)
     {
@@ -71,7 +72,7 @@ abstract class AbstractAdapter extends AbstractValidator implements AdapterInter
      *
      * @param  string $key
      * @param  string $value
-     * @return AbstractAdapter
+     * @return AbstractAdapter Provides a fluent interface
      */
     public function setOption($key, $value)
     {
@@ -97,7 +98,7 @@ abstract class AbstractAdapter extends AbstractValidator implements AdapterInter
      *
      * @param  array|Traversable $options
      * @throws Exception\InvalidArgumentException
-     * @return AbstractAdapter
+     * @return AbstractAdapter Provides a fluent interface
      */
     public function setOptions($options = [])
     {
